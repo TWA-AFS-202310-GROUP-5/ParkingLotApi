1fa3e7cd710b18b2b9ece9b11226687a6635d17d
Yichen Zhang <yzhang183@slb.com>
2023-11-08 13:21:17 +0800

    test: add testbase and test for weatherforecast's correct behavior

---
+++ ParkingLotApiTest/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ParkingLotApiTest
+{
+    public class TestBase : IClassFixture<WebApplicationFactory<Program>>
+    {
+        public TestBase(WebApplicationFactory<Program> factory)
+        {
+            this.Factory = factory;
+        }
+
+        public WebApplicationFactory<Program> Factory { get; }
+
+        protected HttpClient GetClient()
+        {
+            return Factory.CreateClient();
+        }
+    }
+}
---
+++ ParkingLotApiTest/WeatherForecastControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace ParkingLotApiTest
+{
+    public class WeatherForecastControllerTest : TestBase
+    {
+        private HttpClient _httpClient;
+
+        public WeatherForecastControllerTest(WebApplicationFactory<Program> factory) : base(factory)
+        {
+            _httpClient = GetClient();
+        }
+
+        [Fact]
+        public async Task Should_return_correctly_when_get_weather_forecast()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("/WeatherForecast");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
