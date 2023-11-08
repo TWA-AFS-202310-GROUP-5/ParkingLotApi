4296bf55c06d86ade8cf13ed68c9202d34cad825
Zhiping Shu <ZShu@slb.com>
2023-11-08 13:04:06 +0800

    test: add test for get weatherForecast

---
+++ ParkingLotApiTest/Controllers/WeatherForcastControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace ParkingLotApiTest.Controllers
+{
+    public class WeatherForcastControllerTest
+    {
+        private HttpClient _httpClient;
+        public WeatherForcastControllerTest()
+        {
+            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>();
+            _httpClient = webApplicationFactory.CreateClient(); 
+        }
+        [Fact]
+        public async Task Should_return_correctly_when_get_weather_forecast()
+        {
+            // given & when
+            HttpResponseMessage response = await _httpClient.GetAsync("/weatherForecast");
+            // then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
