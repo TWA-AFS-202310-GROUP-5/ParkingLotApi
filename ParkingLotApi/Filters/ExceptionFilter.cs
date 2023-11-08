c377d0e39a4bd67766fc39409bec14892b70f914
Jinglin Wang <JWang317@slb.com>
2023-11-08 18:29:19 +0800

    fix: costum exception not filtered

---
+++ ParkingLotApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ParkingLotApi.Exceptions;
+
+namespace ParkingLotApi.Filters
+{
+    public class ExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        int IOrderedFilter.Order => 2;
+        public int Order => throw new NotImplementedException();
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is InvalidCapacityException)
+            {
+                context.Result = new BadRequestResult();
+                context.ExceptionHandled = true;
+            }
+            if (context.Exception is IDNotExistException)
+            {
+                context.Result = new NotFoundResult();
+                context.ExceptionHandled = true;
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
