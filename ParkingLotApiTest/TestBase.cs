2850d5a82da3940789b45981e80257c25b388b4b
Yang Liu <YLiu148@slb.com>
2023-11-08 13:22:17 +0800

    test: add get all weather forecast test case

---
+++ ParkingLotApiTest/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ParkingLotApiTest
+{
+    public class TestBase : IClassFixture<WebApplicationFactory<Program>>
+    {
+        public TestBase(WebApplicationFactory<Program> factory)
+        {
+            this.Factory = factory;
+        }
+        protected WebApplicationFactory<Program> Factory { get; }
+
+        public HttpClient GetClient()
+        {
+            return Factory.CreateClient();
+        }
+        
+    }
+}
